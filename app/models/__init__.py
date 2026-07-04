63684cb8aee139663564acf3afa81f895675f510
Dezmils <dez.ezra05@gmail.com>
2026-07-04 16:21:25 +0300

    day2-don

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,18 +1,15 @@
-# Export Asset model
-from .asset import Asset
+"""Model package initializer.
 
-# Import other models to make them available
-from .user import User
-from .loan import Loan
-from .subscription_payment import SubscriptionPayment
-from .welfare_event import WelfareEvent
-from .event_contribution import EventContribution
+Importing this package should not eagerly import every document model,
+because that can break startup when a model import fails during packaging
+or deployment. Individual modules are imported directly where needed.
+"""
 
 __all__ = [
+    "Asset",
     "User",
     "Loan",
     "SubscriptionPayment",
     "WelfareEvent",
     "EventContribution",
-    "Asset"
 ]
